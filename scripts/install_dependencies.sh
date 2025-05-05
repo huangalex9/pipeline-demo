493ef3d5b58bfd2bae6b8c1d4ea019768bebcf6c
jaisharmz <jaisharmaus@gmail.com>
2025-05-05 16:27:43 -0700

    python flask minimum example

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 APP_DIR=/home/ec2-user/app
-mkdir -p $APP_DIR
-cd $APP_DIR
+mkdir -p "$APP_DIR"
+# CodeDeploy copied files here, but root owns them. Fix ownership so ec2-user can write.
+sudo chown -R ec2-user:ec2-user "$APP_DIR"
+cd "$APP_DIR"
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
