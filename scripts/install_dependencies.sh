bbc94f9db6e4a95ffabfdccdd3962a2457a4d81c
jaisharmz <jaisharmaus@gmail.com>
2025-05-05 17:07:59 -0700

    requirements.txt not found

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,22 @@ set -e
 APP_DIR=/home/ec2-user/app
 mkdir -p "$APP_DIR"
 cd "$APP_DIR"
+
+python3 -m venv venv
+sudo chown -R ec2-user:ec2-user "$APP_DIR"
+
+# install as ec2-user so packages are writable later
+sudo -u ec2-user bash -c "\
+  source $APP_DIR/venv/bin/activate && \
+  pip install --upgrade pip && \
+  pip install -r requirements.txt\
+"
+```bash
+#!/bin/bash
+set -e
+APP_DIR=/home/ec2-user/app
+mkdir -p "$APP_DIR"
+cd "$APP_DIR"
 python3 -m venv venv
 sudo chown -R ec2-user:ec2-user "$APP_DIR"
 su - ec2-user -c "source $APP_DIR/venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt"
