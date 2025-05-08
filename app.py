9af8a06bdaf257d64786a623b9b7d48ed17649f8
jaisharmz <jaisharmaus@gmail.com>
2025-05-08 13:07:52 -0700

    skill tagging for video data is working (chekpoint)

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ def ask():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
-# skill tagging for video data is working (chekpoint)
+# skill tagging for video data is working (chekpoint) 
