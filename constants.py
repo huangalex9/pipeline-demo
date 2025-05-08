81339839e5723dcb5234ba22b57d1c5876f50fea
jaisharmz <jaisharmaus@gmail.com>
2025-05-08 12:50:53 -0700

    trying to add pipeline

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
-DEFAULT_PROMPT = """
-The video is part of an online learning course. 
-1) Summarize what this video is about, 
-2) Talk about the skills taught,
-3) Explain the difficulty level of those skills from 0 to 5.
-"""
+# DEFAULT_PROMPT = """
+# The video is part of an online learning course. 
+# 1) Summarize what this video is about, 
+# 2) Talk about the skills taught,
+# 3) Explain the difficulty level of those skills from 0 to 5.
+# """
 
 # DEFAULT_PROMPT = "ignore the video and explain the pythagorean theorem."
+DEFAULT_PROMPT = "What is this video about?"
