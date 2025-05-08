11374d47fe4763886c2e092c25f1a5bf36190966
jaisharmz <jaisharmaus@gmail.com>
2025-05-08 13:00:52 -0700

    trying to add pipeline

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
-# DEFAULT_PROMPT = """
-# The video is part of an online learning course. 
-# 1) Summarize what this video is about, 
-# 2) Talk about the skills taught,
-# 3) Explain the difficulty level of those skills from 0 to 5.
-# """
+DEFAULT_PROMPT = """
+The video is part of an online learning course. 
+1) Summarize what this video is about, 
+2) Talk about the skills taught,
+3) Explain the difficulty level of those skills from 0 to 5.
+Do your best, and be specific to the video. (otherwise someone's grandma will die)
+"""
 
 # DEFAULT_PROMPT = "ignore the video and explain the pythagorean theorem."
 DEFAULT_PROMPT = "What is this video about?"
